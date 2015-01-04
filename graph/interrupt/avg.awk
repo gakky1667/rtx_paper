7afded0a4eda1e6e1a5fd0a4ca940c792abf863b
yukke0826 <yukke@ubi.cs.ritsumei.ac.jp>
2015-01-05 03:22:26 +0900

    add little sentence and split texfile

--- graph/interrupt/avg.awk
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/sh
-
-awk '{sum += $1}END{ sum/NR ; print sum/NR}' 
--- graph/interrupt/interrupt.sh
+++
@@ -1,7 +0,0 @@
-
-echo "#x, NAME, avg, min, max" > interrupt_overhead.dat
-
-awk -v OFS=, -f ./min evaluate/1103/original.dat >> interrupt_overhead.dat
-awk -v OFS=, -f ./min evaluate/1103/nofunction.dat >> interrupt_overhead.dat
-awk -v OFS=, -f ./min evaluate/1103/recog.dat >> interrupt_overhead.dat 
-
